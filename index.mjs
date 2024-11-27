8c81efe726e838b7ce7f8c0f6596c2445bf56143
Stoffel-Games-Studio <stoffelgamesstudio@gmail.com>
2024-11-27 10:24:58 +0200

    Create index.mjs

---
+++ index.mjs
@@ -0,0 +1,24 @@
+import "./style.css";
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Fill the canvas, cropping Rive if necessary
+let layout = new rive.Layout({
+    fit: rive.Fit.Cover,
+    });
+        
+const r = new rive.Rive({
+    src: 'https://cdn.rive.app/animations/vehicles.riv',
+    canvas: document.getElementById('canvas'),
+    autoplay: true,
+    stateMachines: 'bumpy',
+    layout: layout,
+    onLoad: (_) => {
+        // Get the inputs via the name of the state machine
+        const inputs = r.stateMachineInputs('bumpy');
+        // Find the input you want to set a value for, or trigger
+        const bumpTrigger = inputs.find(i => i.name === 'bump');
+        button.onclick = () => bumpTrigger.fire();
+    },
+});
