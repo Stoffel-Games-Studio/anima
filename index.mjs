969573f0f3a159099dc27d5236c75e8e0b1ccbb9
Stoffel-Games-Studio <stoffelgamesstudio@gmail.com>
2024-11-27 14:19:09 +0200

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,22 +1,23 @@
+
+const canvas = document.getElementById("canvas");
+const button = document.getElementById("button");
+
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-// Fill the canvas, cropping Rive if necessary
-let layout = new rive.Layout({
-    fit: rive.Fit.Cover,
+const layout = new Layout({
+    fit: Fit.Cover,
 });
 
-const r = new rive.Rive({
-    src: 'https://cdn.rive.app/animations/vehicles.riv',
-    canvas: document.getElementById('canvas'),
+const riveInstance = new Rive({
+    src: "https://cdn.rive.app/animations/vehicles.riv",
+    canvas: canvas,
     autoplay: true,
-    stateMachines: 'bumpy',
+    stateMachines: "bumpy",
     layout: layout,
-    onLoad: (_) => {
-        // Get the inputs via the name of the state machine
-        const inputs = r.stateMachineInputs('bumpy');
-        // Find the input you want to set a value for, or trigger
-        const bumpTrigger = inputs.find(i => i.name === 'bump');
+    onLoad: () => {
+        const inputs = riveInstance.stateMachineInputs("bumpy");
+        const bumpTrigger = inputs.find(i => i.name === "bump");
         button.onclick = () => bumpTrigger.fire();
     },
 });
