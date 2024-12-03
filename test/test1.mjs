c5534a0c611a59a811a60e5e7bd503f443c1ed7f
Stoffel-Games-Studio <stoffelgamesstudio@gmail.com>
2024-12-03 05:33:22 +0200

    Add files via upload

---
+++ test/test1.mjs
@@ -0,0 +1,15 @@
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Fill the canvas, cropping Rive if necessary
+let layout = new rive.Layout({
+    fit: rive.Fit.Cover,
+});
+
+const r = new rive.Rive({
+    src: 'https://stoffel-games-studio.github.io/anima/assets/anima.riv',
+    canvas: document.getElementById('canvas'),
+    autoplay: true,
+    stateMachines: 'sm',
+    layout: layout,
+});
